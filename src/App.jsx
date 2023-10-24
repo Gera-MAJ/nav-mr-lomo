7bd44519b32f24ca70b5938f310ee2b893ca34f4
Gerardo <jatipg@gmail.com>
2023-10-24 01:29:06 -0300

    terminado los menus sin animación

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,14 @@
 import './App.css'
 import logomrlomo from '../img/logo-mr-lomo.png'
 import NavMrLomo from './components/Nav';
-import Dventa from './components/DisplayVenta'
-import Dfactura from './components/DisplayFactura'
-import Dstock from './components/DisplaStock'
-import Dproducto from './components/DisplayProducto'
 
 function App() {
-
-  const Ventas = {
-    primera: "Nueva Venta",
-    segunda: "Ventas Realizadas"
-  }
-
-  const Facturas = {
-    primera: "Facturas Realizadas",
-  }
-
-  const Stock = {
-    primera: "Stock Disponible",
-    segunda: "Aumentar Stock"
-  }
-
-  const Productos = {
-    primera: "Lista de Productos",
-    segunda: "Agregar Producto"
-  }
-
 
   return (
     <>
       <NavMrLomo />
-      <Dventa Info = {Ventas}/>
-      <Dfactura Info = {Facturas}/>
-      <Dstock Info = {Stock}/>
-      <Dproducto Info = {Productos}/>
       
-      <img className='logoLomo' src={logomrlomo} alt="logo"/>
-       
+      <img className='logoLomo' src={logomrlomo} alt="logo"/>    
     </>
   )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,68 @@
-import React from "react"
+import React, { useState }from "react"
 
 export default function nav(){
       
+    const venta = document.querySelector(".DisplayVenta");
+
     function ClickA(item){
         alert(`Click en ${item}`)
     }
 
+    function Click(name){
+        const venta = document.querySelector(".DisplayVenta");
+        const factura = document.querySelector(".DisplayFactura");
+        const stock = document.querySelector(".DisplayStock");
+        const producto = document.querySelector(".DisplayProducto");
+
+        if(name == "venta"){
+            venta.classList.toggle("DisplayVentaActive")
+        } else if (name == "facturas"){
+                factura.classList.toggle("DisplayFacturaActive")
+            }else if (name == "stock"){
+                    stock.classList.toggle("DisplayStockActive")
+                }else if (name == "productos"){
+                    producto.classList.toggle("DisplayProductoActive")
+                }
+    }
+
     return(
-        <nav className='nav'>
+        <>
+            <nav className='nav'>
             <ul>
-                <li><a href="#" onClick={()=>{ClickA('ventas')}}>VENTAS</a></li>
-                <li><a href="#" onClick={()=>{ClickA('facturas')}}>FACTURAS</a></li>
-                <li><a href="#" onClick={()=>{ClickA('stock')}}>STOCK</a></li>
-                <li><a href="#" onClick={()=>{ClickA('productos')}}>PRODUCTOS</a></li>
+                <li><a href="#" onClick={()=>{Click('venta')}}>VENTAS</a></li>
+                <li><a href="#" onClick={()=>{Click('facturas')}}>FACTURAS</a></li>
+                <li><a href="#" onClick={()=>{Click('stock')}}>STOCK</a></li>
+                <li><a href="#" onClick={()=>{Click('productos')}}>PRODUCTOS</a></li>
             </ul>
-        </nav> 
+            </nav> 
+
+            <span className='DisplayVenta'>
+                <ul>
+                    <li><a href="#">Nueva Venta</a></li>
+                    <li><a href="#">Ventas Realizadas</a></li>
+                </ul>
+            </span>
+
+            <span className='DisplayFactura'>
+                <ul>
+                    <li><a href="#">Facturas Realizadas</a></li>
+                </ul>
+            </span>
+
+            <span className='DisplayStock'>
+                <ul>
+                    <li><a href="#">Stock Disponible</a></li>
+                    <li><a href="#">Aumentar Stock</a></li>
+                </ul>
+            </span>
+
+            <span className='DisplayProducto'>
+                <ul>
+                    <li><a href="#">Lista de Productos</a></li>
+                    <li><a href="#">Agregar Producto</a></li>
+                </ul>
+            </span>
+        </>
+        
     )
 }
